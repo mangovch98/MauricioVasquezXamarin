a042a12dd8e6b0b84a927f801ee7fc7b3b051109
Mauricio Vásquez Chalup <mauriciovch98@gmail.com>
2019-05-25 08:47:27 -0700

    notaViewModel

--- Parcial2/app/app/ViewModels/notaViewModels.cs
+++ Parcial2/app/app/ViewModels/notaViewModels.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace app.ViewModels
 {
@@ -11,12 +13,42 @@ namespace app.ViewModels
         public DateTime CreatedDate
         public DateTime ModifiedDate
         bool isrunning;
+        private object thow;
         #region Propiedades
         public string NoteId {
         get {return this.NoteId}
-        set {SetValue}
+        set { SetValue(ref this nota, value); }
         }
+        public string Contents {
+            get { return this.Contents; }
+            set { SetValue(ref this.Contents, value)}
+        }
+
+        public bool IsRunning
+        {
+            get { return this.isrunning; }
+            set { SetValue(ref this.isrunning, value); }
+        }
+        public DateTime CreatedDate
+        { get { return this.CreatedDate; }
+          set { SetValue(ref this.CreatedDate, value); }
+}
+        public DateTime ModifiedDate
+        {
+            get { return this.ModifiedDate; }
+            set { SetValue(ref this.ModifiedDate, value); }
+        }
+
         #endregion
+        #region comandos
+        public ICommand LoginCommand{
+get{return new RelayCommand(Login)}
+}
+private void Login()
+{
+    thow new NoImplementedException();
+}
+#endregion
 
     }
 }
